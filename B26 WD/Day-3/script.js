27c6a0f135c7f99721bfd661a1bb6c6cde4be600
Taufiq4045 <87647852+Taufiq4045@users.noreply.github.com>
2021-07-22 17:37:07 +0530

    Add files via upload

---
+++ B26 WD/Day-3/script.js
@@ -0,0 +1,11 @@
+var obj = [
+  { person: "Name 1", age: "2", company: "GUVI" },
+  { person: "Name 2", age: "5", company: "GUVI geek" },
+  { person: "Name 3", age: "8", company: "GUVI geek network" },
+]
+
+for(var x in obj){
+  console.log(x);
+}
+
+//Output :- Gives the keys of array
