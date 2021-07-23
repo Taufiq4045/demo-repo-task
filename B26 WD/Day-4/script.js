7c1e2976c45a1648c8534afa0dc45d906cada3af
Taufiq4045 <87647852+Taufiq4045@users.noreply.github.com>
2021-07-23 17:49:04 +0530

    Add files via upload

---
+++ B26 WD/Day-4/script.js
@@ -0,0 +1,147 @@
+var Resume = {
+    "basics": {
+      "name": "TAUFIQ .",
+      "label": "Aeronautical Engineer",
+      "picture": "",
+      "email": "taufiq4045@gmail.com",
+      "phone": "+919036794897",
+      "website": "https://mohdtaufiq.com",
+      "summary": "A summery of Taufiq...",
+      "location": {
+        "address": "5375 Mannat Villa",
+         "postalCode": "589635",
+         "city": "Mangalore",
+         "countryCode": "IN",
+         "region": "India"
+      },
+      "profiles": [
+        {
+          "network": "GitHub",
+          "username": "taufiq4045",
+          "url": "https://www.github.com/in/BKgsusGDHbZMUBXxJJPBp4lDU3Gw56juTVjsgKhdjE/"
+        },
+        { 
+          "network": "LinkedIn",
+          "username": "taufiq",
+          "url": "https://www.linkedin.com/in/ACoAACHbZMUBXxJJPBp4lDU3Gw47gvTaaOKZzWE/"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "Jet Airways",
+        "endDate": "2020-10-31",
+        "highlights": ["Awarded Best employee of the Moth"],
+        "position": "Flight Operations Specialist",
+        "startDate": "2018-04-30",
+        "summary": "Good experience",
+        "website": "https://www.linkedin.com/company/11392"
+      },
+      {
+        "company": "Air India Limited",
+        "endDate": "2018-03-31",
+        "highlights": ["Award on Time Management"],
+        "position": "Maintenance Engineer",
+        "startDate": "2016-01-31",
+        "summary": "Highly Trained",
+        "website": "https://www.linkedin.com/company/3751291"
+      }
+    ],
+    "volunteer": [{
+      "organization": "BAIL",
+      "position": "Volunteer",
+      "website": "http://BAIL.com/",
+      "startDate": "2016-01-01",
+      "endDate": "2015-01-01",
+      "summary": "Description...",
+      "highlights": [
+        "Awarded 'Volunteer of the Month'"
+      ]
+    }],
+    "education": [
+      {
+        "institution": "Mangalore Institute of Technology and Engineering, MANGALORE",
+        "area": "Aeronautical/Aerospace Engineering Technology/Technician",
+        "studyType": "Bachelor of Engineering",
+        "startDate": "2013-12-31",
+        "endDate": "2017-12-31",
+        "gpa": "7.5",
+        "courses": ["C - Ansys"]
+      }
+    ],
+    "awards": [{
+      "title": "Best Employee",
+      "date": "2020-11-01",
+      "awarder": "Jet Airways",
+      "summary": "There is no spoon."
+    },
+    {
+      "title": "Time Management",
+      "date": "2018-11-01",
+      "awarder": "Air India",
+      "summary": "There is no spoon."
+    }],
+    "publications": [{
+      "name": "Publication",
+      "publisher": "Company",
+      "releaseDate": "2014-10-01",
+      "website": "http://publication.com",
+      "summary": "Description..."
+    }],
+    "skills": [
+      {
+        "name": "CATIA",
+        "level": "Inter-mediate",
+        "keywords": ["Designing"]
+      },
+      {
+        "name": "ANSYS",
+        "level": "Expert",
+        "keywords": ["Modeling"]
+      },
+      {
+        "name": "Simulink",
+        "level": "Master",
+        "keywords": ["Testing"]
+      },
+      {
+        "name": "Aircraft Maintenance",
+        "level": "Master",
+        "keywords": ["maintenance"]
+      }
+    ],
+    "languages": [
+      {
+        "language": "English",
+        "fluency": "Native Speaker"
+      },
+      {
+        "language": "Hindi",
+        "fluency": "Native Speaker"
+      }
+    ],
+    "interests": [{
+      "name": "Sports",
+      "keywords": [
+        "FootBall",
+        "UCricket"
+      ]
+    }],
+    "references": [{
+      "name": "Taufiq",
+      "reference": "Reference..."
+    }]
+  }
+  
+  
+  
+  console.log(Resume);
+  
+
+                
+ 
+
+
+
+
+   
