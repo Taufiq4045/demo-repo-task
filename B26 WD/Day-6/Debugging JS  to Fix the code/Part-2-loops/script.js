0d968838b97d7f70d5e35d962228ee467b163ee1
Taufiq4045 <87647852+Taufiq4045@users.noreply.github.com>
2021-07-26 18:53:40 +0530

    Add files via upload

---
+++ B26 WD/Day-6/Debugging JS  to Fix the code/Part-2-loops/script.js
@@ -0,0 +1,30 @@
+//   Find the culprits and nail them — debugging javascript loops
+var numsArr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+var new_string1 = "";
+ 
+for (var i = 0; i < 11; i++) {
+ new_string1 += numsArr[i];
+}
+console.log(new_string1);
+
+// Output: 1234567891011
+
+var new_string2 = numsArr[0];
+for (var i = 1; i < 11; i++) {
+  new_string2 += "," + numsArr[i];
+ }
+ console.log(new_string2.trim());
+ 
+ // Output: 1,2,3,4,5,6,7,8,9,10,11
+
+
+var new_string3 = "";
+ 
+for (var i = 10; i >= 0; i-- ) {
+ new_string3 += numsArr[i]+ " ";
+}
+console.log(new_string3.trim());
+
+// Output: 1110987654321
+
+
