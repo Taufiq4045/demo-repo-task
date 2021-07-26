e685959e1d4e0a85d764800cf843b88333725c8f
Taufiq4045 <87647852+Taufiq4045@users.noreply.github.com>
2021-07-26 22:20:17 +0530

    Add files via upload

---
+++ B26 WD/Day-6/Debugging JS  to Fix the code/Part-3-functions/script.js
@@ -0,0 +1,17 @@
+// Find the culprit and nail them - JS debugging functions 
+
+const newArray=[1,3,2,5,10];
+
+const myPrime=newArray.filter(num=>{
+ for(let i=2;i<num;i++){
+ if(num % i === 0)
+ {
+ return false;
+ }
+  }
+  return num>1;
+});
+console.log(myPrime);
+
+ // Output :- Prime numbers
+ 
