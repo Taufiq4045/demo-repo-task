afc9c0950d261172b55718676aa6ca9e1904b2d1
Taufiq4045 <87647852+Taufiq4045@users.noreply.github.com>
2021-07-27 21:43:48 +0530

    Add files via upload

---
+++ B26 WD/Day-6/Debugging JS  to Fix the code/Part-2-loops/JavaScript.js
@@ -0,0 +1,14 @@
+//   Find the culprits and nail them — debugging javascript loops
+var numsArr = [[1, 2, 3, 4, 5],[ 6, 7, 8, 9, 10, 11]];
+
+for (var i = 0; i < numsArr.length; i++) {
+ var inner_array = numsArr[i];
+ for(var j = 0 ; j < inner_array.length;j++)
+      if(j % 2 === 0 )
+      {
+         inner_array[j] = "even";
+       }
+}
+console.log(numsArr);
+
+// Output :- Elements in an inner array
