655f97cf06ea1e3ab1fbe8b56d5c80f7d7054f0e
Taufiq4045 <87647852+Taufiq4045@users.noreply.github.com>
2021-07-27 19:43:13 +0530

    Add files via upload

---
+++ B26 WD/Day-6/Debugging JS  to Fix the code/Part-2-loops/script.js
@@ -0,0 +1,9 @@
+// Find the culprit and nail them - JS debugging loops 
+
+var numsArr = [[1, 2, 3, 4, 5],[ 6, 7, 8, 9, 10, 11]];
+for (var i = 0; i < numsArr.length; i++) {
+ console.log(numsArr[i]);
+}
+
+ // Output :- Inner arrays
+ 
