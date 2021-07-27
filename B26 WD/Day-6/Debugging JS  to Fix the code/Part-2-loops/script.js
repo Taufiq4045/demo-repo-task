d1380c6d8d567f435fe830b6a2e9da5e78a0c2a3
Taufiq4045 <87647852+Taufiq4045@users.noreply.github.com>
2021-07-27 18:56:18 +0530

    Add files via upload

---
+++ B26 WD/Day-6/Debugging JS  to Fix the code/Part-2-loops/script.js
@@ -0,0 +1,13 @@
+// Find the culprit and nail them - JS debugging loops 
+
+var numsArr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+var sum=0;
+for (var i = 0; i <=10; i++) {
+  if(numsArr[i] % 2 === 0)
+ {sum += numsArr[i]};
+}
+console.log(sum);
+
+ // Output :- Sum of all even elements in an array
+ 
