c132f57c842fdfef737d90371badc7b082ad3c0b
Taufiq4045 <87647852+Taufiq4045@users.noreply.github.com>
2021-07-29 19:05:36 +0530

    Update Countries from Asia Region using filter function.js

--- B26 WD/Day-9/Countries from Asia Region using filter function.js
+++ B26 WD/Day-9/Countries from Asia Region using filter function.js
@@ -1,6 +1,6 @@
 // Code to get countries from Asia region
 fetch("https://restcountries.eu/rest/v2/all").then((resp) => resp.json()).then((result) => {
-   let tt = result.filter(result => result.region == "Asia")
+   let tt = result.filter(result => result.region === "Asia")
    console.log(tt);
 }).catch((err) => console.log(err));
 
