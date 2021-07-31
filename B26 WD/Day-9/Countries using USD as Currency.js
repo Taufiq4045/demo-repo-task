4f0f92af47ecba9d5f3ad541a6e29d3a4227d1d0
Taufiq4045 <87647852+Taufiq4045@users.noreply.github.com>
2021-07-31 15:28:33 +0530

    Update and rename JavaScript.js to Countries using USD as Currency.js

--- B26 WD/Day-9/Countries using USD as Currency.js
+++ B26 WD/Day-9/Countries using USD as Currency.js
@@ -1,3 +1,4 @@
+// Code to get all the countries using USD as currnecy
 fetch("https://restcountries.eu/rest/v2/all").then((resp) => resp.json()).then((result) => {
     var tt = result.filter(result => result.currencies[0].code === "USD")
     var rr = result.filter(result => result.currencies[1])
@@ -9,6 +10,7 @@ fetch("https://restcountries.eu/rest/v2/all").then((resp) => resp.json()).then((
   
 }).catch((err) => console.log(err));
 
+// Output = Countries using USD as currency
 
 
 
