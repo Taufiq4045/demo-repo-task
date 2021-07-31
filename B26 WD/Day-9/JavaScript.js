c7ea0be9a8087d8f4d4bdc37962e35a04663e594
Taufiq4045 <87647852+Taufiq4045@users.noreply.github.com>
2021-07-31 15:27:00 +0530

    Add files via upload

---
+++ B26 WD/Day-9/JavaScript.js
@@ -0,0 +1,15 @@
+fetch("https://restcountries.eu/rest/v2/all").then((resp) => resp.json()).then((result) => {
+    var tt = result.filter(result => result.currencies[0].code === "USD")
+    var rr = result.filter(result => result.currencies[1])
+    var aa = rr.filter(rr => rr.currencies[1].code === "USD")
+    var uu = result.filter(result => result.currencies[3])
+    var bb = uu.filter(uu => uu.currencies[7].code === "USD")
+    console.log([...tt,...aa,...bb]);
+  
+  
+}).catch((err) => console.log(err));
+
+
+
+
+
